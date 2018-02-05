b8feb5f4ef0e74d9ecceaec3728180c690911eb2
Kory33 <6561358+kory33@users.noreply.github.com>
2018-02-06 01:00:31 +0800

    add InventoryGUIListener class

---
+++ src/main/kotlin/click/seichi/giganticlib/listener/InventoryGUIListener.kt
@@ -0,0 +1,30 @@
+package click.seichi.giganticlib.listener
+
+import click.seichi.giganticlib.gui.InventoryGUISession
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryCloseEvent
+import org.bukkit.event.inventory.InventoryEvent
+import org.bukkit.event.inventory.InventoryOpenEvent
+
+/**
+ * @author unicroak, kory33
+ */
+class InventoryGUIListener : Listener {
+
+    private val InventoryEvent.guiSession
+        get() = inventory?.holder as? InventoryGUISession
+
+    @EventHandler
+    fun onInventoryClick(event: InventoryClickEvent) = event.guiSession?.let { session ->
+        session.getBoundReaction(event.slot)(event)
+    }
+
+    @EventHandler
+    fun onInventoryClose(event: InventoryCloseEvent) = event.guiSession?.onInventoryClose(event)
+
+    @EventHandler
+    fun onInventoryOpen(event: InventoryOpenEvent) = event.guiSession?.onInventoryOpen(event)
+
+}
