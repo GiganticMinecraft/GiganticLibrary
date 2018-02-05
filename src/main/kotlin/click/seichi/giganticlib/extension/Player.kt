16047a9a2d05a79dbfdec2f8c2ebb84de7ced7fa
unicroak <unicroak@gmail.com>
2018-02-06 06:16:11 +0900

    Add Player#spawnColoredParticle

--- src/main/kotlin/click/seichi/giganticlib/extension/Player.kt
+++ src/main/kotlin/click/seichi/giganticlib/extension/Player.kt
@@ -1,6 +1,7 @@
 package click.seichi.giganticlib.extension
 
 import click.seichi.giganticlib.util.particle.NoiseData
+import org.bukkit.Color
 import org.bukkit.Location
 import org.bukkit.Particle
 import org.bukkit.entity.Player
@@ -10,7 +11,7 @@ import org.bukkit.entity.Player
  *
  * @param particleType the type of particle to spawn
  * @param location the location to spawn
- * @param count the number of particles
+ * @param count the number of the particles
  * @param noiseData the data to add noise to the location
  * @param speed the extra data for this particle which depends on the particle used (normally speed)
  * @param data the extra data for this particle whose type depends on [Particle.dataType] (normally `null`)
@@ -32,5 +33,32 @@ fun Player.spawnParticle(
             0.0,
             speed,
             data
+    )
+}
+
+/**
+ * Spawns the colored particles which can be seen only by this [Player].
+ *
+ * The type of this particle is determined [Particle.REDSTONE].
+ *
+ * @param color the color of particle to spawn
+ * @param location the location to spawn
+ * @param count the number of the particles
+ * @param noiseData the data to add noise to the location
+ */
+fun Player.spawnColoredParticle(
+        color: Color,
+        location: Location,
+        count: Int = 1,
+        noiseData: NoiseData = NoiseData(0.0)
+) = (0 until count).forEach {
+    spawnParticle(
+            Particle.REDSTONE,
+            location.noisedWith(noiseData),
+            0,
+            color.red.toDouble() / 255.0 + 0.001,
+            color.green.toDouble() / 255.0,
+            color.blue.toDouble() / 255.0,
+            1.0
     )
 }
