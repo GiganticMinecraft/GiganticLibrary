1e6cfcbb8e5c98f8439992860af9b75fc1ca7564
Kory33 <6561358+kory33@users.noreply.github.com>
2018-02-09 02:22:24 +0800

    make InventoryGUIListener pseudo-singleton

--- src/main/kotlin/click/seichi/giganticlib/listener/InventoryGUIListener.kt
+++ src/main/kotlin/click/seichi/giganticlib/listener/InventoryGUIListener.kt
@@ -1,6 +1,7 @@
 package click.seichi.giganticlib.listener
 
 import click.seichi.giganticlib.gui.session.InventoryGUISession
+import click.seichi.giganticlib.listener.InventoryGUIListener.Companion.isInstanceAlreadyListening
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
@@ -11,12 +12,23 @@ import org.bukkit.event.inventory.InventoryOpenEvent
 /**
  * A default implementation of a listener which conveys events to the [InventoryGUISession].
  *
- * Note that this class is most likely to cause a conflict between multiple plugins
- * when used without the package relocation.
+ * Do check for return value of [isInstanceAlreadyListening] because multiple registration causes
+ * multiple event notification to the session instance.
+ *
+ * When [isInstanceAlreadyListening] is `true`, the constructor throws an exception
+ * to ensure that no multiple plugins register instances of this class.
+ *
+ * Do not try to register the listener except from the main thread as it would break the single registration principle.
  *
  * @author unicroak, kory33
  */
 class InventoryGUIListener : Listener {
+
+    init {
+        if (isInstanceAlreadyListening()) {
+            throw IllegalStateException("The listener is already registered!")
+        }
+    }
 
     private val InventoryEvent.guiSession
         get() = inventory?.holder as? InventoryGUISession
@@ -31,5 +43,14 @@ class InventoryGUIListener : Listener {
 
     @EventHandler
     fun onInventoryOpen(event: InventoryOpenEvent) = event.guiSession?.onInventoryOpen(event)
+
+    companion object {
+        /**
+         * Returns true if an instance of this class is already registered to the bukkit's event system.
+         */
+        fun isInstanceAlreadyListening() = InventoryClickEvent.getHandlerList().registeredListeners.any {
+            it.listener is InventoryGUIListener
+        }
+    }
 
 }
